fe50a99b4a9ea11f3b05dfc575ca05edf88ea158
SIVUPE <sivuyisiwepeter79@gmail.com>
2023-11-24 15:04:55 +0200

    content

---
+++ script/code.js
@@ -0,0 +1,18 @@
+function convertToseconds(minutes) {
+    var perMinutes = 60;
+    var perSeconds = 60;
+    var totalSec = documents.getElementById("OnInput").innerHTML
+    
+    var minutes = documents.getElementById("minutes").value;
+    var Seconds = +documents.getElementById("Seconds").value;
+    
+    document.getElementById("calculate").innerHTML;
+}
+
+
+
+// console.log(toSeconds("1:29.460"));   // returns 89.460
+
+
+
+ 
